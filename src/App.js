4bece4ecf3775c091c7cdc82662c21f8dce08af9
Tina Hang <mstinahang@gmail.com>
2020-02-26 04:57:52 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -15,13 +15,6 @@ class App extends Component {
     this.handleAddTimer()
   }
 
-
-
-
-
-
-
-
   // No need to modify anything in render or the class methods below
   // Unless, of course, you're curious about how it all works
   render() {
